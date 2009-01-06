e644bbf2047dd24f834fd954ead456ea33dad63a
tomhebbron <tom@hebbron.com>
2009-01-06 01:03:55 +0000

    not quite yet working NKalpha_sort2.cpp
    git-svn-id: svn+ssh://forge.ecs.soton.ac.uk/projects/tom-hebbron-phd/tehPG@354 14e2aa37-a43d-0410-ad94-f2a3057bbd13

--- tehpg.c
+++ tehpg.c
@@ -9,64 +9,69 @@
 PG_MODULE_MAGIC;
 #endif
 
-Datum popcount( PG_FUNCTION_ARGS );
-PG_FUNCTION_INFO_V1( popcount );
+Datum popcount(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(popcount);
 
-Datum popcount( PG_FUNCTION_ARGS )
+Datum popcount(PG_FUNCTION_ARGS)
 {
-  VarBit *arg1 = PG_GETARG_VARBIT_P(0);
-  int32 result;
-  int len, i;
-  bits8 *b;
-  bits8 mask;
-  len = VARBITLEN(arg1);
-  b = VARBITS(arg1);
-  result = 0;
-  for(i=0; i< VARBITBYTES(arg1); i++)
-    {
-      mask = *b++;
-      while (mask)
-        {
-          if (mask & 1) result++;
-          mask >>= 1;
-        }
-    }
-  PG_RETURN_INT32(result);
+	VarBit *arg1 = PG_GETARG_VARBIT_P(0);
+	int32 result;
+	int len, i;
+	bits8 *b;
+	bits8 mask;
+	len = VARBITLEN(arg1);
+	b = VARBITS(arg1);
+	result = 0;
+
+	for (i=0; i< VARBITBYTES(arg1); i++)
+	{
+		mask = *b++;
+		while (mask)
+		{
+			if (mask & 1) result++;
+			mask >>= 1;
+		}
+	}
+	PG_RETURN_INT32(result);
 }
 
 
-Datum shuffled_ints( PG_FUNCTION_ARGS );
-PG_FUNCTION_INFO_V1( shuffled_ints );
+Datum shuffled_ints(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(shuffled_ints);
 
-Datum shuffled_ints( PG_FUNCTION_ARGS )
+Datum shuffled_ints(PG_FUNCTION_ARGS)
 {
-  ArrayType *result;
-  Datum *nums;
-  long i, swap, limit;
-  int64 tmp;
-  limit = (long) PG_GETARG_INT64(0);
-  nums = (Datum *)palloc(limit * sizeof(Datum));
-  if (limit > 0)
-    {
-      for(i=0; i < limit; i++)
-        {
-          //elog( NOTICE, "%i", i );
-          nums[i] = Int64GetDatum(i);
-        }
-      //elog(NOTICE,"finished building the array");
-      //now shuffle
-      for (i=limit-1; i >=0; i--)
-        {
-          swap = random()%(long)limit;
-          //elog(NOTICE, "%i %i",i,swap);
-          tmp = DatumGetInt64(nums[swap]);
-          nums[swap] = nums[i];
-          nums[i] = Int64GetDatum(tmp);
-        }
-    }
-  result = construct_array(nums, limit, INT8OID, sizeof(int64), false,'d');
-  pfree(nums);
-  PG_RETURN_ARRAYTYPE_P(result);
+	ArrayType *result;
+	Datum *nums;
+	long i, swap, limit;
+	int64 tmp;
+	limit = (long) PG_GETARG_INT64(0);
+	nums = (Datum *)palloc(limit * sizeof(Datum));
+
+	if (limit > 0)
+	{
+		for (i=0; i < limit; i++)
+		{
+			//elog( NOTICE, "%i", i );
+			nums[i] = Int64GetDatum(i);
+		}
+
+		//elog(NOTICE,"finished building the array");
+		//now shuffle
+		for (i=limit-1; i >=0; i--)
+		{
+			swap = random()%(long)limit;
+			//elog(NOTICE, "%i %i",i,swap);
+			tmp = DatumGetInt64(nums[swap]);
+			nums[swap] = nums[i];
+			nums[i] = Int64GetDatum(tmp);
+		}
+	}
+
+	result = construct_array(nums, limit, INT8OID, sizeof(int64), false,'d');
+
+	pfree(nums);
+	PG_RETURN_ARRAYTYPE_P(result);
 }
 
 
